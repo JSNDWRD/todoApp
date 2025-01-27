1177acdae16a573dba28e39e74962508c9f4a116
JSNDWRD <jasonedwardsalim@gmail.com>
2025-01-27 17:48:08 +0700

    minor fixes

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import { prisma } from "../utils/prisma";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   const todos = await prisma.todos.findMany();
   return NextResponse.json(todos);
 }
