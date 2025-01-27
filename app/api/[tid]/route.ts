fc79df1e7ffeba60490611b4a174e8dc42c369db
JSNDWRD <jasonedwardsalim@gmail.com>
2025-01-27 18:10:05 +0700

    fixes

--- app/api/[tid]/route.ts
+++ app/api/[tid]/route.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function PUT(
   req: NextRequest,
-  { params }: { params: { tid: string } }
+  { params }: { params: Promise<{ tid: string }> } // Type the params correctly
 ) {
   const body = await req.json();
   const updateTodo = await prisma.todos.update({
-    where: { id: parseInt(params.tid, 10) },
+    where: { id: parseInt((await params).tid, 10) },
     data: {
       description: body.description,
     },
@@ -17,20 +17,20 @@ export async function PUT(
 
 export async function GET(
   req: NextRequest,
-  { params: { tid } }: { params: { tid: string } }
+  { params }: { params: Promise<{ tid: string }> } // Type the params correctly
 ) {
   const todo = await prisma.todos.findUnique({
-    where: { id: parseInt(await tid, 10) },
+    where: { id: parseInt((await params).tid, 10) },
   });
   return NextResponse.json(todo);
 }
 
 export async function DELETE(
   req: NextRequest,
-  { params: { tid } }: { params: { tid: string } }
+  { params }: { params: Promise<{ tid: string }> } // Type the params correctly
 ) {
   const deleteTodo = await prisma.todos.delete({
-    where: { id: parseInt(await tid, 10) },
+    where: { id: parseInt((await params).tid, 10) },
   });
   return NextResponse.json(deleteTodo);
 }
